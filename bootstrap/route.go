a4f085098fc51eb587ae89d81b57358e387e1af5
gopherer <wuhe092@gmail.com>
2022-12-11 07:09:37 +0800

    配置方案的实现

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -3,7 +3,7 @@ package bootstrap
 
 import (
 	"github.com/gin-gonic/gin"
-	"graproject/routes"
+	"gohub/routes"
 	"net/http"
 	"strings"
 )
--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"graproject/bootstrap"
+	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
@@ -14,7 +28,8 @@ func main() {
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
 
-	err := router.Run(":3000")
+	// 运行服务
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
